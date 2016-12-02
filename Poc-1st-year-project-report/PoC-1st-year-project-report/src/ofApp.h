a10fe2564d51bb81bf3afd46cd5e3b8bf3f265c1
firdauslubis88_1344 <chemfirus@gmail.com>
2016-12-02 18:07:39 +0900

    Adding the correct folder

---
+++ Poc-1st-year-project-report/PoC-1st-year-project-report/src/ofApp.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxGui.h"
+#include "ofxOpenVR.h"
+
+#include "CombinedCamera.h"
+
+class ofApp : public ofBaseApp {
+
+public:
+	void setup();
+	void exit();
+
+	void update();
+	void draw();
+
+	void render(vr::Hmd_Eye nEye);
+
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y);
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void mouseEntered(int x, int y);
+	void mouseExited(int x, int y);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
+
+	void onToggle(const void* sender);
+
+	ofxOpenVR openVR;
+
+	bool bShowHelp;
+	std::ostringstream _strHelp;
+
+	ofFbo ldFbo;
+	ofFbo hdFbo;
+	ofVboMesh sphereVboMesh;
+	ofEasyCam _easyCam;
+	ofImage ldImage, hdImage, combinedImage, ldPassImage;
+	vector<ofVideoDevice> listVideoDevice;
+	ofShader _shader;
+	ofVideoGrabber ldVideoGrabber;
+	ofVideoGrabber hdVideoGrabber;
+	bool isldCameraConnected;
+	bool isHdCameraConnected;
+	bool isCombined;
+
+	ofShader shader;
+	ofSpherePrimitive sphere;
+
+	ofxPanel _panel;
+	ofParameter<ofVec4f> offsetParameter;
+	ofParameter<float> radiusParameter;
+	ofParameterGroup ldParameterGroup;
+	ofxButton ldToggle;
+	ofxButton hdToggle;
+	ofxButton combinedToggle;
+
+	int VIDEO_WIDTH = 1280, VIDEO_HEIGHT = 640;
+	string cameraSelected;
+
+	CombinedCamera combinedCamera = CombinedCamera(VIDEO_WIDTH,VIDEO_HEIGHT);
+	ofPixels ldPixels;
+};
