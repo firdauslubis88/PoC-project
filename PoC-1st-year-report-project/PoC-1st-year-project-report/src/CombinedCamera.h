d31a886f150a98bd7f1d980647cb3398865c56c3
firdauslubis88_1344 <chemfirus@gmail.com>
2016-12-02 18:11:33 +0900

    Adding correct files

---
+++ PoC-1st-year-report-project/PoC-1st-year-project-report/src/CombinedCamera.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "ofImage.h"
+#include "ofxOpenCv.h"
+#include "Alignment.h"
+#include "Cloning.h"
+
+using namespace cv;
+
+class CombinedCamera
+{
+public:
+	CombinedCamera(int image_width, int image_height);
+	~CombinedCamera();
+
+//	void seamlessClone(InputArray _src, InputArray _dst, InputArray _mask, Point p, OutputArray _blend, int flags);
+
+	ofPixels combine(ofPixels ldPixels, ofImage hdImage, int image_width, int image_height, int x, int y, int width, int height);
+	void setSkipCloning(bool value);
+
+	void setSkipAligning(bool value);
+
+private:
+	std::shared_ptr<ofxCvImage> combinedImage;
+	ofxCvColorImage ldCvImage, hdCvImage, combinedCvImage, sourceCvImage;
+	bool skipCloning = false, skipAligning = false;
+};
